822322f8a73491e290aec306aadef6cb19ef642f
VVPolyukhov <vlad.polyukhov@gmail.com>
2022-03-13 20:35:07 +0300

    Тестовый коммит 3

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Стажировка по фронтенду (группа 3)	&#128293;</p>
+        <p>&#128293; Стажировка по фронтенду (группа 3)	&#128293;</p>
         <button type={"button"} onClick={authHandler}>
           СДЕЛАТЬ ЗАПРОС НА АВТОРИЗАЦИЮ ПОЛЬЗОВАТЕЛЯ С ID=1
         </button>
