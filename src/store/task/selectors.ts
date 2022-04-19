04275ae52cb6277532c9c9d5e338b55f2dd2de24
jvedernikova <julia.vedernikova@freemake.com>
2022-04-19 19:52:50 +0300

    add createSelector

--- src/store/task/selectors.ts
+++ src/store/task/selectors.ts
@@ -1,3 +1,4 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'store';
 import { IAssignUser, IRoles, ITaskRoles } from './types';
 
@@ -85,31 +86,90 @@ function getUsersIdFromRoles(obj:Array<ITaskRoles>|undefined){
   return arr;
 } */
 
+const allroles = (state: RootState) => state.onetask.allroles;
+const taskData = (state: RootState) => state.onetask.data;
+const taskRoles = (state: RootState) => state.onetask.data.roles;
 
-// export const getTask = (state: RootState) => state.onetask.data;
-export const getWatcherRoleID = (state: RootState) =>state.onetask.allroles?.find(isWatcherFromRoles)?.task_role_id;
-export const getResponsibleRoleID = (state: RootState) =>state.onetask.allroles?.find(isResponsibleFromRoles)?.task_role_id;
-export const getImplementerRoleID = (state: RootState) =>state.onetask.allroles?.find(isImplementerFromRoles)?.task_role_id;
+export const getWatcherRoleID = createSelector(
+  allroles,
+  roles => roles?.find(isWatcherFromRoles)?.task_role_id
+);
+export const getResponsibleRoleID = createSelector(
+  allroles,
+  roles => roles?.find(isResponsibleFromRoles)?.task_role_id
+);
+export const getImplementerRoleID = createSelector(
+  allroles,
+  roles => roles?.find(isImplementerFromRoles)?.task_role_id
+);
+
+
+export const getTaskId = createSelector(
+  taskData,
+  data => data.task_id
+);
+export const getTitle = createSelector(
+  taskData,
+  data => data.title
+);
+export const getDescription = createSelector(
+  taskData,
+  data => data.description
+);
+export const getTaskStatus = createSelector(
+  taskData,
+  data => data.status.name
+);
+
+
+
+export const getTaskAuthor = createSelector(
+  taskRoles,
+  roles => roles?.find(isAuthor)?.assign_user
+);
+export const getTaskResponsible = createSelector(
+  taskRoles,
+  roles => roles?.find(isResponsible)?.assign_user
+);
+export const getTaskImplementer = createSelector(
+  taskRoles,
+  roles => roles?.find(isImplementer)?.assign_user
+);
+export const getTaskWatchers = createSelector(
+  taskRoles,
+  roles => getUsersFromRoles(roles?.filter(isWatcher))
+);
+export const getTaskWatchersID = createSelector(
+  taskRoles,
+  roles => getUsersIdFromRoles(roles?.filter(isWatcher))
+);
+
+
 export const getMembers = (state: RootState) => state.onetask?.members;
 export const getTaskLoading = (state: RootState) => state.onetask.loading;
 export const getTaskError = (state: RootState) => state.onetask.error;
-
-export const getTaskId = (state: RootState) => state.onetask.data?.task_id;
-export const getTitle = (state: RootState) => state.onetask.data?.title;
-export const getDescription = (state: RootState) => state.onetask.data?.description;
-export const getTaskStatus = (state: RootState) => state.onetask.data?.status.name;
-
 export const getNewSelectedMembers = (state: RootState) => state.onetask?.selectedMembers; 
-export const getUnselectedMembers = (state: RootState) => state.onetask?.unselectedMembers; 
+export const getUnselectedMembers = (state: RootState) => state.onetask?.unselectedMembers;
 
-export const getTaskAuthor = (state: RootState) => state.onetask.data?.roles?.find(isAuthor)?.assign_user;
-export const getTaskResponsible = (state: RootState) => state.onetask.data?.roles?.find(isResponsible)?.assign_user;
-export const getTaskImplementer = (state: RootState) => state.onetask.data?.roles?.find(isImplementer)?.assign_user;
-export const getTaskWatchers = (state: RootState) => getUsersFromRoles(state.onetask.data?.roles?.filter(isWatcher));
-export const getTaskWatchersID = (state: RootState) =>getUsersIdFromRoles(state.onetask.data?.roles?.filter(isWatcher));
+
+
+// export const getTask = (state: RootState) => state.onetask.data;
+// export const getWatcherRoleID = (state: RootState) =>state.onetask.allroles?.find(isWatcherFromRoles)?.task_role_id;
+// export const getResponsibleRoleID = (state: RootState) =>state.onetask.allroles?.find(isResponsibleFromRoles)?.task_role_id;
+// export const getImplementerRoleID = (state: RootState) =>state.onetask.allroles?.find(isImplementerFromRoles)?.task_role_id;
+
+// export const getTaskId = (state: RootState) => state.onetask.data?.task_id;
+// export const getTitle = (state: RootState) => state.onetask.data?.title;
+// export const getDescription = (state: RootState) => state.onetask.data?.description;
+// export const getTaskStatus = (state: RootState) => state.onetask.data?.status.name;
+
+// export const getTaskAuthor = (state: RootState) => state.onetask.data?.roles?.find(isAuthor)?.assign_user;
+// export const getTaskResponsible = (state: RootState) => state.onetask.data?.roles?.find(isResponsible)?.assign_user;
+// export const getTaskImplementer = (state: RootState) => state.onetask.data?.roles?.find(isImplementer)?.assign_user;
+// export const getTaskWatchers = (state: RootState) => getUsersFromRoles(state.onetask.data?.roles?.filter(isWatcher));
+// export const getTaskWatchersID = (state: RootState) =>getUsersIdFromRoles(state.onetask.data?.roles?.filter(isWatcher));
 // export const getTaskResponsibleID = (state: RootState) => state.onetask.data?.roles?.find(isResponsible)?.assign_user.user_id;
 // export const getTaskImplementerID = (state: RootState) => state.onetask.data?.roles?.find(isImplementer)?.assign_user.user_id;
-
 
 /* function isCreate(element) {
   if (element.name !== "Создана") {
