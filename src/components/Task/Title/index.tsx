c530bd183095fd35ea2c3574298e9fc73886694b
jvedernikova <julia.vedernikova@freemake.com>
2022-05-03 13:48:42 +0300

    styles & focus for title

--- src/components/Task/Title/index.tsx
+++ src/components/Task/Title/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import TextArea from 'antd/lib/input/TextArea';
 import { getTaskId, getTitle } from 'store/editTask/selectors';
 import { useAppSelector } from 'customHooks/redux/useAppSelector';
 import { setTaskTitle } from 'store/editTask/thunk';
 import { useAppDispatch } from 'customHooks/redux/useAppDispatch';
 import { EditOutlined } from '@ant-design/icons';
+import classnames from 'classnames';
 import styles from './index.module.scss';
 
 const Title: React.FC = () => {
@@ -12,14 +13,23 @@ const Title: React.FC = () => {
   const title = useAppSelector(getTitle);
   const taskId = useAppSelector(getTaskId);
 
+  const inputRef = useRef<any>(null);
   const [newTitle, setNewTitle] = useState<string | undefined>(title);
+  const [isReadonly, setIsReadonly] = useState<boolean>(true);
+
+  const changeReadonly = () => {
+    setIsReadonly(!isReadonly);
+    inputRef.current!.focus({
+      cursor: 'end',
+    });
+  };
 
   const changeTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNewTitle(e.target.value);
   };
 
   const onBlur = () => {
-    if (taskId && newTitle) {
+    if (taskId && newTitle && !isReadonly) {
       dispatch(setTaskTitle({ task_id: taskId, title: newTitle }));
     }
   };
@@ -28,16 +38,20 @@ const Title: React.FC = () => {
     <>
       <div className={styles.wrapname}>
         <TextArea
+          ref={inputRef}
           autoSize
           maxLength={150}
           placeholder="Введите название"
-          className={styles.name}
+          className={classnames(styles.name, {
+            [styles.readonly]: isReadonly,
+          })}
           onChange={changeTitle}
           onBlur={onBlur}
           value={newTitle || ''}
           status={!newTitle ? 'error' : ''}
+          readOnly={isReadonly}
         />
-        <EditOutlined />
+        {isReadonly ? <EditOutlined onClick={changeReadonly} /> : null}
       </div>
       <div className={styles.border} />
     </>
