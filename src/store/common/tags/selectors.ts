c69dc9b85dae78156557d598c00d693357022790
Arbuznik <arbyzov.nikita@yandex.ru>
2022-05-04 04:14:47 +0300

    render only unique participants and tags

--- src/store/common/tags/selectors.ts
+++ src/store/common/tags/selectors.ts
@@ -1,14 +1,27 @@
 import { RootState } from 'store/index';
 import { createSelector } from '@reduxjs/toolkit';
-import lodash from 'lodash';
 import { IPopulatedTag, ITag } from './types';
 
 const selectTags = (state: RootState): Array<ITag> => state.common.tags.tags;
 
+const selectUniqueTags = createSelector(selectTags, (tags): Array<ITag> => {
+  const uniqueTagsIds: Array<string> = [];
+  const uniqueTags: Array<ITag> = [];
+
+  tags.forEach((tag) => {
+    if (!uniqueTagsIds.includes(tag.task_tag_id)) {
+      uniqueTagsIds.push(tag.task_tag_id);
+      uniqueTags.push(tag);
+    }
+  });
+
+  return uniqueTags;
+});
+
 export const selectPopulatedTags = createSelector(
-  selectTags,
+  selectUniqueTags,
   (tags): Array<IPopulatedTag> =>
     tags.map((tag) => {
-      return { ...tag, value: tag.name, key: lodash.uniqueId() };
+      return { ...tag, value: tag.name, key: tag.task_tag_id };
     }),
 );
