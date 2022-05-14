848d9dad1a2070d4edcd2422ab5d07701d95ee45
jvedernikova <julia.vedernikova@freemake.com>
2022-05-14 16:12:59 +0300

    fix routing

--- src/containers/Routes/PrivateRoute/index.tsx
+++ src/containers/Routes/PrivateRoute/index.tsx
@@ -2,9 +2,18 @@ import React, { ReactElement } from 'react';
 import { useAppSelector } from 'customHooks/redux/useAppSelector';
 import { Navigate } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
-import { getVerifyLoading } from 'store/auth/verify/selectors';
-import { getAuthLoading, getVerifyToken } from 'store/auth/token/selectors';
+import {
+  getVerifyError,
+  getVerifyIdUser,
+  getVerifyLoading,
+} from 'store/auth/verify/selectors';
+import {
+  getAuthError,
+  getAuthLoading,
+  getVerifyToken,
+} from 'store/auth/token/selectors';
 import Preloader from 'components/Common/Preloader';
+import { getToken } from 'helpers/cookies';
 
 interface IRouteProps {
   children: ReactElement;
@@ -15,18 +24,41 @@ const PrivateRoute: React.FC = ({ children: Component }: IRouteProps) => {
   const authLoading = useAppSelector(getAuthLoading);
   const loading = verifyLoading || authLoading;
   const verifyToken = useAppSelector(getVerifyToken);
+  const userID = useAppSelector(getVerifyIdUser);
+  const token = getToken();
+  const verifyError = useAppSelector(getVerifyError);
+  const authError = useAppSelector(getAuthError);
+  const error = verifyError || authError;
+  // const [isBeenLoad, setIsBeenLoad] = useState<boolean>(false);
 
-  return (
+  /* if (loading && !isBeenLoad) {
+    setIsBeenLoad(true);
+  } */
+
+  if (
+    error ||
+    !token /* (!loading && isBeenLoad && !userID && !verifyToken) */
+  ) {
+    return <Navigate to={ROUTES.login.path} />;
+  }
+
+  if (loading || (!verifyToken && userID)) {
+    return <Preloader size="large" />;
+  }
+
+  return <>{verifyToken ? Component : null}</>;
+
+  /* return (
     <>
       {loading ? (
         <Preloader size="large" />
       ) : verifyToken ? (
         Component
       ) : (
-        <Navigate to={ROUTES.login.path} />
+        <Preloader size="large" />
       )}
     </>
-  );
+  ); */
 };
 
 export default PrivateRoute;
