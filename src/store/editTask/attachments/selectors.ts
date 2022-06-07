6c86600a3ddeebe5ada8bef5656bb9a57f56666d
Irin <iraantonova1998@list.ru>
2022-06-07 11:43:44 +0300

    fix: использование createSelector

--- src/store/editTask/attachments/selectors.ts
+++ src/store/editTask/attachments/selectors.ts
@@ -1,15 +1,20 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'store/index';
 
 export const isClickedAttachments = (state: RootState) =>
   state.editTask.attachments.isClicked;
 
-export const getStorageFile = (state: RootState) =>
-  state.editTask.attachments.data?.map(({ name_original, storage_file_id }) => {
+export const attachmentsData = (state: RootState) =>
+  state.editTask.attachments.data;
+
+export const getStorageFile = createSelector(attachmentsData, (attachments) =>
+  attachments.map(({ name_original, storage_file_id }) => {
     return {
       name: name_original,
       storageId: storage_file_id,
     };
-  });
+  }),
+);
 
 export const getfileName = (state: RootState) =>
   state.editTask.attachments.data?.map(({ name_original: name }) => name);
