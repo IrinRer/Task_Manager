e0c2d786475f0cb6cf064a8ad9b81b9b974405b4
jvedernikova <julia.vedernikova@freemake.com>
2022-06-14 16:45:42 +0300

    add rights in home

--- src/components/Home/Block/Options/OptionsMenu/index.tsx
+++ src/components/Home/Block/Options/OptionsMenu/index.tsx
@@ -2,39 +2,70 @@ import React, { useContext } from 'react';
 import { Button, notification } from 'antd';
 import { useAppDispatch } from 'customHooks/redux/useAppDispatch';
 import { useAppSelector } from 'customHooks/redux/useAppSelector';
-import { canUserDuplicateTask, isUserTaskAuthor } from 'helpers/userRoles';
-import { getVerifyIdUser } from 'store/auth/verify/selectors';
 import { cloneTaskAction } from 'store/createTask/thunk';
 import { deleteTaskAction } from 'store/tasks/thunk';
 import { TaskContext } from 'constants/taskContext';
+import { getMyMaxRoleForTask } from 'store/common/roles/selectors';
+import { getRights } from 'helpers/rights';
+import { RIGHTS_NAMES } from 'constants/rights';
+import { ROLES } from 'constants/types/common';
 import styles from './index.module.scss';
 
 const OptionsMenu: React.FC = () => {
   const dispatch = useAppDispatch();
-  const userId = useAppSelector(getVerifyIdUser);
   const task = useContext(TaskContext);
 
+  const myMaxRoleFromAllTask = useAppSelector((state) =>
+    task ? getMyMaxRoleForTask(state, task) : ROLES.any,
+  );
+  const isRightsCopyTask = getRights(
+    myMaxRoleFromAllTask,
+    RIGHTS_NAMES.copyTask,
+  );
+  const isRightsArchiveTask = getRights(
+    myMaxRoleFromAllTask,
+    RIGHTS_NAMES.moveToArchive,
+  );
+  const isRightsDelTask = getRights(
+    myMaxRoleFromAllTask,
+    RIGHTS_NAMES.deleteTask,
+  );
+
   const handleCloneTask = (): void => {
-    if (task && canUserDuplicateTask(userId, task)) {
+    if (task) {
       dispatch(cloneTaskAction({ id: task.task_id, edit: false }));
     } else notification.warn({ message: 'Нет прав на дублирование задачи' });
   };
 
   const handleDeleteTask = (): void => {
-    if (task && isUserTaskAuthor(userId, task)) {
+    if (task) {
       dispatch(deleteTaskAction(task.task_id));
     } else notification.warn({ message: 'Удалить задачу может только автор' });
   };
 
   return (
     <div className={styles.wrapper}>
-      <Button className={styles.button} type="text" onClick={handleCloneTask}>
+      <Button
+        disabled={!isRightsCopyTask}
+        className={styles.button}
+        type="text"
+        onClick={handleCloneTask}
+      >
         Дублировать задачу
       </Button>
-      <Button className={styles.button} type="text">
+      <Button
+        disabled={!isRightsArchiveTask}
+        className={styles.button}
+        type="text"
+      >
         Отслеживать задачу
       </Button>
-      <Button className={styles.button} type="text" onClick={handleDeleteTask}>
+      <Button
+        disabled={!isRightsDelTask}
+        className={styles.button}
+        type="text"
+        onClick={handleDeleteTask}
+      >
         Удалить задачу
       </Button>
     </div>
