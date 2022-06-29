a32cc999ee16fea8d5f6872c7eae4b76317966dd
jjuliee <jjuliee@yandex.ru>
2022-06-29 14:38:38 +0300

    add counter for memebers

--- src/components/Task/Members/OneMember/index.tsx
+++ src/components/Task/Members/OneMember/index.tsx
@@ -10,9 +10,10 @@ import styles from './index.module.scss';
 
 type TProps = {
   user?: IUser;
+  count?: number;
 };
 
-const OneMember: FC<TProps> = ({ user }) => {
+const OneMember: FC<TProps> = ({ user, count }) => {
   const roleName = useContext(RightsRoleContext).role;
   const editable = useContext(RightsRoleContext).isRights;
   const usersData = useMembersProps(roleName);
@@ -25,12 +26,13 @@ const OneMember: FC<TProps> = ({ user }) => {
       className={classnames(
         user ? styles.inlist : styles.nolist,
         !editable ? styles.noedit : '',
+        count ? styles.counterWrapper : '',
       )}
     >
       {editable && member ? <EditableMember user={member} /> : null}
 
       {!editable && member ? <MemberItem obj={member} /> : null}
-
+      {count && <span className={styles.counter}>{`+${count}`}</span>}
       {!member && editable ? <AddMemberButton /> : null}
     </div>
   );
